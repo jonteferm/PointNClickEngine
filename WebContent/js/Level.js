60b6403a7207a5ae4f92a277bb4495d9e2b283fb
JonteFerm <jonte_ferm@hotmail.com>
2018-08-02 14:23:32 +0200

    Loading an actual background image

---
+++ WebContent/js/Level.js
@@ -0,0 +1,24 @@
+/**
+ * Level state.
+ */
+function Level() {
+	Phaser.State.call(this);
+}
+
+/** @type Phaser.State */
+var proto = Object.create(Phaser.State.prototype);
+Level.prototype = proto;
+Level.prototype.constructor = Level;
+
+Level.prototype = {
+	create: function(){
+		var background = this.game.add.tileSprite(0, 0, 1024, 768, 'testfoto1');
+	},
+
+	update: function(){
+	
+	},
+
+};
+
+
